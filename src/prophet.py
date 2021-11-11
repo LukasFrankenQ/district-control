2330e5816106e8ec784b333f67742f829f577fb0
FRANKEN Lukas <s2216495@ed.ac.uk>
2021-11-11 15:32:36 +0000

    added data handling

--- src/prophet.py
+++ src/prophet.py
@@ -107,17 +107,14 @@ class BaseProphet:
     even though two modes exist, from the outside, the class has the 
     exact same functionalities independent of the mode
     
-
     Attributes
     ----------
     mode : str
         'adversarial' for data based time series with noise
         'predict' for machine learning based time series
-
     data : pd.Series
         full time series available from which a small exempt is
         taken at every call
-
     noise_scale : float 
         (relevant only for mode = 'adversarial')
         determines how much fluctuation the returned time series is subject to
@@ -126,10 +123,8 @@ class BaseProphet:
         i.e. Let x_t be the t-th entry of time series of quantity x.
              entry x_0 will be free of noise, entry x_t will be subject
              to gaussian noise with standard deviation t*noise_scale
-
-    steps : int
+    num_steps : int
         length of returned time series
-
     model : tbd
         machine learning model that returns time series
 
@@ -138,13 +133,19 @@ class BaseProphet:
     predict(**args)
         returns next time series
     
-    setup(mode, data=None, model=None)
-        based on mode sets up data for mode='adversarial' and model for 
-        mode='predict'
+    setup_adversarial(data)
+        obtains self.data from which chunks will be returned
+
+    setup_prediction(model)
+        sets up saved model for prediction
 
     """
-    def __init__(self, mode, data=None, model=None):
+    def __init__(self, mode, data=None, model=None, num_steps=10, 
+                       noise_scale=0.05):
         """
+        inits the class
+        Also sets up the data or model that should be returned
+
         Parameters
         ----------
         mode : str
@@ -154,17 +155,19 @@ class BaseProphet:
             necessary if mode is 'adversarial'
         model : str or tbd model type 
             necessary if mode is 'predict'
-
+        num_steps : int
+            length of time series that will be returned in predict call
+        noise_scale : float
+            std of gaussian noise that is added every time step
 
         Returns
         ----------
         -
-
-        inits the class
-        Also sets up the data or model that should be returned
         """
 
         self.mode = mode
+        self.num_steps = num_steps
+        self.noise_scale = noise_scale
 
         if mode == 'adversarial': 
             assert data is not None, f'for chosen mode adversarial, \
@@ -177,7 +180,6 @@ class BaseProphet:
 
         if mode == 'adversarial':
             self.setup_adversarial(data) 
-             
 
         elif mode == 'predict':
             self.setup_predict(model)
@@ -186,15 +188,78 @@ class BaseProphet:
 
     def setup_adversarial(self, data):
         """
-        Either from file path or from pd.Series, sets up full 
+        Either from file path or from pd.Series, sets up full available time series 
 
+        Parameters
+        ----------
+        data : str or Series
+            full data from which smaller exempts will be drawn at each 'prediction'
+        
+        Returns
+        ----------
+        -
 
         """
+        if isinstance(data, str):
+            print('Set up adversarial prophet from file {}!'.format(data)) 
+            data = pd.read_csv(data)
+            self.data = data[data.columns[-1]]
+
+        elif isinstance(data, pd.Series):
+            print('Set up adversarial prophet from pd.Series!')
+            self.data = data
+
+        elif isinstance(data, pd.DataFrame):
+            print('Set up adversarial prophet from pd.DataFrame!')
+            self.data = data[data.columns[-1]]
+
+
+    def setup_predict(self, model):
+        """
+        Either from file or from object sets up machine learning model
+
+        Parameters
+        ----------
+        model : str or tbd
+
+        Returns
+        ----------
+        -
+        """
+        raise NotImplementedError('implement me!')
+
+    
+    def predict(self, **state):
+        """
+        Returns prediction based on current state of the system
+
+        Parameters
+        ----------
+        state : dict
+            reflects current system state
+        
+        Returns
+        ----------
+        y : np.array
+            vector of predictions
+        """
+        if self.mode == 'adversarial':
+
+            assert 't' in state, 'Passed systems state for prediction must contain t. \
+                                  Currently contains {}'.format(state)
+
+            t = state['t']
+
+            # create cumulative random noise
+            noise = np.random.normal(scale=self.noise_scale, size=self.num_steps)
+            noise = np.array([noise[:i+1].sum() for i in range(self.num_steps)])
+
+            return pd.Series(self.data[t:t+self.num_steps].to_numpy() + noise)
+            
 
 
 
 if __name__ == '__main__':
-
     # for testing purposes
     # create artificial time series
     df = pd.DataFrame({'a': np.sin(np.linspace(0, 20, 1000))})
@@ -203,7 +268,15 @@ if __name__ == '__main__':
     series.to_csv(filename)
 
     # test for pass of filename
-    prophet = BaseProphet('adversarialll', data='filename')
+    prophet = BaseProphet('adversarial', data=series, noise_scale=0.01)
+    
+    for i in range(10):
+        ts = prophet.predict(t=i)
+        fig, ax = plt.subplots(1, 1, figsize=(10, 7))
+        ts.plot(ax=ax)
+        plt.show()
+
+
 
 
 
